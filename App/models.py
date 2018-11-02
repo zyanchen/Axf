f51057adb134b610ac8e5a9a2a54a6c91ff9f1af
zyanchen <761543013@qq.com>
2018-11-02 20:54:28 +0800

    首页完成

--- App/models.py
+++ App/models.py
@@ -19,3 +19,48 @@ class Nav(Base):
 class Mustbuy(Base):
     class Meta:
         db_table = 'axf_mustbuy'
+
+class Shop(Base):
+    class Meta:
+        db_table = 'axf_shop'
+
+
+class MainShow(models.Model):
+    trackid = models.CharField(max_length=8)
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=100)
+    categoryid = models.CharField(max_length=100)
+    brandname = models.CharField(max_length=50)
+
+    img1 = models.CharField(max_length=100)
+    childcid1 = models.CharField(max_length=8)
+    productid1 = models.CharField(max_length=8)
+    longname1 = models.CharField(max_length=100)
+    price1 = models.FloatField()
+    marketprice1 = models.FloatField()
+
+    img2 = models.CharField(max_length=100)
+    childcid2 = models.CharField(max_length=8)
+    productid2 = models.CharField(max_length=8)
+    longname2 = models.CharField(max_length=100)
+    price2 = models.FloatField()
+    marketprice2 = models.FloatField()
+
+    img3 = models.CharField(max_length=100)
+    childcid3 = models.CharField(max_length=8)
+    productid3 = models.CharField(max_length=8)
+    longname3 = models.CharField(max_length=100)
+    price3 = models.FloatField()
+    marketprice3 = models.FloatField()
+
+    class Meta:
+        db_table = 'axf_mainshow'
+
+class Foodtypes(models.Model):
+    typeid = models.CharField(max_length=8)
+    typename = models.CharField(max_length=100)
+    childtypenames = models.CharField(max_length=256)
+    typesort = models.IntegerField()
+
+    class Meta:
+        db_table = 'axf_foodtypes'
--- App/views.py
+++ App/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
-from App.models import Wheel, Nav, Mustbuy
+from App.models import Wheel, Nav, Mustbuy, Shop, MainShow, Foodtypes
 
 
 def index(request):
     wheels = Wheel.objects.all()
     navs = Nav.objects.all()
     musts = Mustbuy.objects.all()
+    shoplist = Shop.objects.all()
+    shophead = shoplist[0]
+    shoptab = shoplist[1:3]
+    shopclass = shoplist[3:7]
+    shopcommend = shoplist[7:11]
+
+    mainshows = MainShow.objects.all()
+
     data = {
         'wheels':wheels,
         'navs':navs,
         'musts':musts,
+        'shophead':shophead,
+        'shoptab':shoptab,
+        'shopclass':shopclass,
+        'shopcommend':shopcommend,
+        'mainshows':mainshows,
     }
     return render(request,'home/home.html',context=data)
 
@@ -21,7 +34,14 @@ def cart(request):
 
 
 def market(request):
-    return render(request,'market/market.html')
+
+    foodtypes = Foodtypes.objects.all()
+
+    data = {
+        'foodtypes':foodtypes,
+    }
+
+    return render(request,'market/market.html',context=data)
 
 
 def mine(request):
